f17408fafc8c03ed6e8acf545bbc0b69e31cff3c
Robin Bron <finwo@pm.me>
2020-06-17 11:35:19 +0200

    Based malloc on sbrk now

--- include/malloc.h
+++ include/malloc.h
@@ -3,7 +3,9 @@
 
 #include <stddef.h>
 
-void * malloc(size_t n);
+void *malloc(size_t n);
 void free(void *p);
+void *realloc(void *ptr, size_t size);
+void *calloc(size_t nelem, size_t elsize);
 
 #endif // _MALLOC_H_
--- include/string.h
+++ include/string.h
@@ -4,7 +4,7 @@
 #include <stddef.h>
 
 int memcmp (const void *str1, const void *str2, int count);
-void * memcpy (void *dest, const void *src, size_t len);
-void * memset (void *dest, int val, size_t len);
+void *memcpy (void *dest, const void *src, size_t len);
+void *memset (void *dest, int val, size_t len);
 
 #endif // _STRING_H_
