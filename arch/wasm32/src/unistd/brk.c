443afd8fd08dc87f6495ec74eb21b98fe0ec26eb
Robin Bron <finwo@pm.me>
2021-08-18 18:07:27 +0200

    sbrk now calls upon __builtin_wasm_memory_grow for wasm32

--- arch/wasm32/src/unistd/brk.c
+++ arch/wasm32/src/unistd/brk.c
@@ -1,7 +1,10 @@
 #ifndef _UNISTD_BRK_C_
 #define _UNISTD_BRK_C_
 
+#include <stddef.h>
 #include <unistd.h>
+
+#include <matter/page_size.h>
 
 extern unsigned char __heap_base;
 void *break_pointer = &__heap_base;
@@ -12,8 +15,16 @@ int brk(void *addr) {
 }
 
 void * sbrk(ssize_t increment) {
+  size_t current_pages = __builtin_wasm_memory_size(0);
+  void   *heap_limit   = current_pages * PAGE_SIZE;
+
   void *ret = break_pointer;
   break_pointer += increment;
+
+  if (break_pointer > heap_limit) {
+    __builtin_wasm_memory_grow(0, ((break_pointer - heap_limit) / PAGE_SIZE) + 1);
+  }
+
   return ret;
 }
 
